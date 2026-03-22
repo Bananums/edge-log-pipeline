6c5f34b74db8916b8f015bf65d4b92fc83d94acf
Bananums <sjohnsen.valle@gmail.com>
2026-03-22 21:24:48 +0100

    Just some playing around. Changed info color.

--- app/src/logger.cc
+++ app/src/logger.cc
@@ -9,26 +9,29 @@
 #include "quill/sinks/JsonSink.h"
 
 void QuillLogTest() {
-    // Start the backend thread
-    quill::BackendOptions backend_options;
-    quill::Backend::start(backend_options);
+  // Start the backend thread
+  quill::BackendOptions backend_options;
+  quill::Backend::start(backend_options);
+
+  quill::FileSinkConfig file_cfg;
+  file_cfg.set_open_mode('w');
+  file_cfg.set_filename_append_option(quill::FilenameAppendOption::None);
+
+  quill::ConsoleSinkConfig console_cfg;
+  quill::ConsoleSinkConfig::Colours colours;
+  colours.assign_colour_to_log_level(quill::LogLevel::Info, quill::ConsoleSinkConfig::Colours::blue);
+  console_cfg.set_colours(colours);
 
   // Create a json file for output
   auto json_sink = quill::Frontend::create_or_get_sink<quill::JsonFileSink>(
     "example_json.log",
-    []()
-    {
-      quill::FileSinkConfig cfg;
-      cfg.set_open_mode('w');
-      cfg.set_filename_append_option(quill::FilenameAppendOption::None);
-      return cfg;
-    }(),
+    file_cfg,
     quill::FileEventNotifier{});
 
   // It is also possible to create a logger than logs to both the json file and stdout
   // with the appropriate format
   auto json_sink_2 = quill::Frontend::get_sink("example_json.log");
-  auto console_sink = quill::Frontend::create_or_get_sink<quill::ConsoleSink>("console_sink_id_1");
+  auto console_sink = quill::Frontend::create_or_get_sink<quill::ConsoleSink>("console_sink_id_1", console_cfg);
 
   // Define a custom format pattern for console logging, which includes named arguments in the output.
   // If you prefer to omit named arguments from the log messages, you can remove the "[%(named_args)]" part.
@@ -42,12 +45,16 @@ void QuillLogTest() {
   quill::Logger* hybrid_logger = quill::Frontend::create_or_get_logger(
     "hybrid_logger", {std::move(json_sink_2), std::move(console_sink)}, console_log_pattern);
 
-  for (int i = 2; i < 4; ++i)
-  {
+  for (int i = 2; i < 4; ++i){
     LOG_INFO(hybrid_logger, "{method} to {endpoint} took {elapsed} ms", "POST", "http://", 10 * i);
   }
 
   LOG_INFO(hybrid_logger, "Operation {name} completed with code {code}", "Update", 123,
            "Data synced successfully");
+
+  LOG_DEBUG(hybrid_logger, "This is debug", "");
+  LOG_INFO(hybrid_logger, "This is info", "");
+  LOG_WARNING(hybrid_logger, "This is warning", "");
+  LOG_ERROR(hybrid_logger, "This is error", "");
 
 }
